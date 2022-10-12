57333c205b6464aa8da854ee5be2606cc735b6bb
Ivan Rudnichenki <rudnicenko.ivan@gmail.com>
2022-10-12 12:48:08 +0300

    made homepage

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,15 +1,16 @@
 //CSS
 import "./homepage.scss";
 import {Link} from "react-router-dom";
-
 function Homepage(params) {
 	return(
 		<main className="main">
-			<img src="https://content.asos-media.com/-/media/homepages/unisex/generic-hp/august-2022/2022_unihp_desktop.jpg" alt="cover"
-			className="main__cover"
-			/>
-			<div className="main__container container">
-				<Link to="/goods" className="main__btn btn-main">Go to catalog</Link>
+			<div className="main__container">
+				<img src="https://content.asos-media.com/-/media/homepages/unisex/generic-hp/august-2022/2022_unihp_desktop.jpg" alt="cover"
+				className="main__cover"
+				/>
+				<div className="main__text">
+					<Link to="/goods" className="main__btn btn-main">До каталогу</Link>
+				</div>
 			</div>
 		</main>
 	)
